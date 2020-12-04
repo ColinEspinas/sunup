d7a86665175d0a8d2f1b9d6faee01f06491c56f3
ColinEspinas <colin.espinas@yahoo.fr>
2020-12-04 18:38:47 +0100

    docs/chore: Added package.json for npm publish and improved sunup entry file

--- src/sunup.js
+++ src/sunup.js
@@ -1,3 +1,16 @@
+/*! Sunup v0.1.0 | MIT License | github.com/ColinEspinas/sunup */
+/**
+ *   ,pP"Ybd `7MM  `7MM  `7MMpMMMb.`7MM  `7MM `7MMpdMAo. 
+ *   8I   `"   MM    MM    MM    MM  MM    MM   MM   `Wb 
+ *   `YMMMa.   MM    MM    MM    MM  MM    MM   MM    M8 
+ *   L.   I8   MM    MM    MM    MM  MM    MM   MM   ,AP 
+ *   M9mmmP'   `Mbod"YML..JMML  JMML.`Mbod"YML. MMbmmd'  
+ *                                              MM       
+ *                                            .JMML.     
+ * 
+ *   This is the global export file for the sunup framework.
+ */
+
 import define from './define.js';
 import useState from './state.js';
 import useProps from './props.js';
@@ -17,4 +30,5 @@ export {
 	JSONToCSS
 };
 
+// Default not used as of today but exported to handle possible importing errors
 export default undefined;
